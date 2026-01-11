7ca85343deef3ddfac55dbd70a8d0ba119588501
Tomáš Greguš <gregustomas@seznam.cz>
2026-01-11 20:07:41 +0100

    feat: today main ui

--- src/pages/TodayPage.jsx
+++ src/pages/TodayPage.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import StatCard from "../components/StatCard";
 import FocusCard from "../components/FocusCard";
 
+const focuses = [
+  { id: "1", title: "Focus1", time: 90, project: "Momentum", status: "active" },
+  { id: "2", title: "Focus2", status: "skipped" },
+  { id: "3", title: "Focus3", time: 30, project: "Gym", status: "completed" },
+];
+
 function TodayPage() {
-  // return <div className="rounded-2xl bg-white p-6 shadow-sm">Today page</div>;
+  const activeFocuses = focuses.filter((f) => f.status === "active");
+  const doneFocuses = focuses.filter(
+    (f) => f.status === "skipped" || f.status === "completed"
+  );
   const date = new Date();
   const weekday = date.toLocaleDateString("cs-CZ", {
     weekday: "long",
@@ -16,6 +25,7 @@ function TodayPage() {
 
   return (
     <div>
+      {/* header */}
       <div className="flex items-center justify-between mb-8">
         <div className="block">
           <p className="text-sm font-medium text-slate-500 mb-1 capitalize">
@@ -47,10 +57,11 @@ function TodayPage() {
           />
         </div>
       </div>
+      {/* add focus */}
       <div className="flex items-center justify-between mb-6">
         <div className="flex items-center gap-3">
           <div className="w-10 h-10 rounded-xl bg-indigo-100 flex items-center justify-center">
-            <i class="fi fi-rr-sparkles text-indigo-500"></i>
+            <i className="fi fi-rr-sparkles text-indigo-500"></i>
           </div>
           <div>
             <h2 className="text-xl font-semibold text-slate-900">
@@ -62,20 +73,38 @@ function TodayPage() {
           </div>
         </div>
         <button className="cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 h-9 px-4 py-2 bg-slate-900 hover:bg-slate-800 text-white shadow-sm">
-          <i className={"fi fi-rr-plus lucide lucide-plus w-4 h-4 mr-2"}></i>
+          <i className={"fi fi-rr-plus w-4 h-4 mr-2"}></i>
           Add Focus
         </button>
       </div>
+      {/* focuses list */}
       <div className="space-y-4">
-        <FocusCard status="active" />
+        {activeFocuses.length > 0 &&
+          activeFocuses.map((f) => (
+            <FocusCard
+              key={f.id}
+              title={f.title}
+              time={f.time}
+              project={f.project}
+              status={f.status}
+            />
+          ))}
       </div>
       <div className="mt-8 pt-6 border-t border-slate-200">
         <h3 className="text-sm font-medium text-slate-500 mb-4">
           Completed & Skipped
         </h3>
         <div className="space-y-3">
-          <FocusCard status="skipped" />
-          <FocusCard status="completed" />
+          {doneFocuses.length > 0 &&
+            doneFocuses.map((f) => (
+              <FocusCard
+                key={f.id}
+                title={f.title}
+                time={f.time}
+                project={f.project}
+                status={f.status}
+              />
+            ))}
         </div>
       </div>
     </div>
