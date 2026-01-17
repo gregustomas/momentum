87747dedf438c2c7c8e5d39d2c31416090bc1394
Tomáš Greguš <gregustomas@seznam.cz>
2026-01-17 19:17:41 +0100

    feat: add focus countdown timer

--- src/pages/FocusPage.jsx
+++ src/pages/FocusPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CircularTimer from "../components/CircularTimer";
 
@@ -11,16 +11,43 @@ const mainBtnStyle =
 const mainBtnStyleActive =
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 px-8 w-20 h-20 rounded-full text-white shadow-lg transition-all bg-amber-500 hover:bg-amber-600 shadow-amber-500/30";
 
-function FocusPage({ title = "Focus", time }) {
+function formatTime(seconds) {
+  const m = Math.floor(seconds / 60);
+  const s = seconds % 60;
+  return `${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
+}
+
+function FocusPage({ title = "Focus", time=45 }) {
   const [isActive, setIsActive] = useState(false);
+  const totalSeconds = time * 60;
+  const [remainingSeconds, setRemainingSeconds] = useState(totalSeconds);
+
+  useEffect(() => {
+    if (!isActive) return;
+
+    if (remainingSeconds <= 0) {
+      setIsActive(false);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setRemainingSeconds((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [remainingSeconds, isActive]);
 
   return (
-    <div className="px-6 py-12 flex items-center justify-center w-full h-full inset-0 fixed bg-indigo-900 p-6 shadow-sm">
+    <div className="px-6 py-12 flex items-center justify-center w-full h-full inset-0 fixed bg-indigo-950 p-6 shadow-sm">
       <div className="flex flex-col items-center">
         <h1 className="text-3xl md:text-4xl font-bold text-white mb-12 max-w-xl text-center mx-auto leading-tight">
           {title}
         </h1>
-        <CircularTimer timeText="00:00" label="Focus Time" progress={0.0} />
+        <CircularTimer
+          timeText={formatTime(remainingSeconds)}
+          label="Focus Time"
+          progress={0.0}
+        />
         <div className="flex justify-center items-center gap-4 mt-12">
           <button className={btnStyle}>
             <i class="fi fi-rr-rotate-right"></i>
